840879c1af32ba04a9109cd3c69b6c2acd1f3b87
mfallon1 <mfallon1@my.wctc.edu>
2019-12-13 03:21:57 +0000

    12/12

--- MFFinal/Cat/DeleteCat.cs
+++ MFFinal/Cat/DeleteCat.cs
@@ -10,15 +10,17 @@ namespace MFFinal
     internal class DeleteCat
     {
         private static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+        int id;
+
         public DeleteCat()
         {
             var db = new NorthwindContext();
             //string newname;
             //string newdesc;
             //
-            Console.WriteLine("Select the category you want to edit:");
+            Console.WriteLine("Select the category you want to Delete:");
 
-            int id = int.Parse(DisplayCat.DispCatSel()); // display the list of categories
+            id = int.Parse(DisplayCat.DispCatSel()); // display the list of categories
 
             //var query = db.Categories.OrderBy(p => p.CategoryId);
 
@@ -33,41 +35,30 @@ namespace MFFinal
 
             // get the record
             Category category = db.Categories.FirstOrDefault(c => c.CategoryId == id); // get the record context - is connected to the database - have to get the context first to update it
-            Console.WriteLine($"You chose {category.CategoryName} - {category.Description}");
+            //Console.WriteLine($"You selected: {category.CategoryName} - {category.Description}");
             logger.Info($" {category.CategoryName} - {category.Description}");
 
             var cquery = db.Products.Where(p => p.CategoryId == id).Count();
-            Console.WriteLine($"\n** {cquery} Product(s) will be deleted\n");
+            Console.WriteLine($"\n** You selected: {category.CategoryName} {category.Description} - with {cquery} Product(s)\n");
 
-            Console.WriteLine("Edit Category Name? Y/N");
-            string edit = Console.ReadLine();
-            logger.Info($"{edit}");
+            Console.WriteLine("Delete the Category and its Products? Y/N");
+            string del = Console.ReadLine();
+            logger.Info($"{del}");
 
 
-            if (edit.ToLower() == "y")
+            if (del.ToUpper() == "Y")
             {
-                Console.WriteLine("Enter a new Category name");
-                newname = Console.ReadLine(); // get the name 
-                logger.Info($"{newname}");
-                category.CategoryName = newname;
+                var dquery = db.Products.Where(p => p.CategoryId == id);
+                foreach (var item in dquery)
+                {
+                    db.Products.Remove(item);
+                    //db.SaveChanges();
+                }
+                var delcat = db.Categories.FirstOrDefault(c => c.CategoryId == id);
+                db.Categories.Remove(delcat);
+                db.SaveChanges();
             }
-
-            Console.WriteLine("Edit Category Description? Y/N");
-            edit = Console.ReadLine();
-            logger.Info($"{edit}");
-
-            if (edit.ToLower() == "y")
-            {
-                Console.WriteLine("Enter new Category description");
-                newdesc = Console.ReadLine();
-                category.Description = newdesc;
-                logger.Info($"{newdesc}");
-            }
-            // update naem and db - this works because we have it from above
-            //    db.Categories.Add(category);
-            db.SaveChanges();
-
         }
     }
-    }
+
 }
